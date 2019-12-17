23c35feba465c80e098f8329c9c1f99f16108c77
Robert Tudor <marketing@robert-tudor.com>
2019-12-17 16:54:55 +0000

    corrected schema definition

--- src/migrations/2019_12_17_001_create_categories_table.php
+++ src/migrations/2019_12_17_001_create_categories_table.php
@@ -14,9 +14,9 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->engine("InnoDB");
-            $table->charset("utf8");
-            $table->collation("utf8_unicode_ci");
+            $table->engine = "InnoDB";
+            $table->charset = "utf8";
+            $table->collation = "utf8_unicode_ci";
             $table->increments('id');
             $table->integer('owid')->unsigned()->unique('unique_owid');
             $table->string('name',255);
