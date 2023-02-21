3c3a1a2750b99355a5d86b59ff0ac7d75429e8e4
Robert <rtudor@palmcentre.co.uk>
2023-02-21 11:11:41 +0200

    bumped up laravel version and adjusted models, added .gitignore

--- src/Category.php
+++ src/Category.php
@@ -5,6 +5,9 @@ namespace Mcpuishor\Greenberrycatalog;
 use Mcpuishor\Greenberrycatalog\Category;
 
 use Illuminate\Database\Eloquent\Model,
+	Illuminate\Database\Eloquent\Relations\HasMany,
+	Illuminate\Database\Eloquent\Relations\HasManyThrough,
+	Illuminate\Database\Eloquent\Relations\BelongsTo,
 	Mcpuishor\Greenberrycatalog\Product,
     Mcpuishor\Greenberrycatalog\Variant;
 
@@ -12,22 +15,24 @@ class Category extends Model
 {
 	protected $fillable = ["owid", "name"];
 
-    public function products()
+    public function products() : HasMany
     {
     	return $this->hasMany(Product::class);
     }
 
-    public function variants()
+    public function variants() : HasManyThrough
     {
-        return $this->hasManyThrough(Variants::class, Product::class);
+        return $this->hasManyThrough(Variant::class, Product::class);
     }
 
-    public function parent() {
-        return $this->belongsTo(Category::class, "parent_id", "id");
+    public function parent() : BelongsTo
+    {
+        return $this->belongsTo(__CLASS__, "parent_id", "id");
     }
 
-    public function children() {
-        return $this->hasMany(Category::class, "parent_id", "id");
+    public function children() : HasMany
+    {
+        return $this->hasMany(__CLASS__, "parent_id", "id");
     }
 
     /* mutators to deal with the import of string values from Orderwise */
